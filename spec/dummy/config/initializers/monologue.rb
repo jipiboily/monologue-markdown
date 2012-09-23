d61318f7f8c44cb3ca73f47fec31d0edf6e66049
Jean-Philippe Boily <jipiboily@gmail.com>
2012-09-23 19:12:25 -0400

    add some dummy config

---
+++ spec/dummy/config/initializers/monologue.rb
@@ -0,0 +1,16 @@
+Monologue.site_name            = "@jipiboily / Jean-Philippe Boily"
+Monologue.site_subtitle        = "my own place online"
+Monologue.site_url             = "http://jipiboily.com"
+Monologue.disqus_shortname     = "jipiboily"
+Monologue.meta_description     = "This is my personal blog about Rails, Monologue, programming, etc..."
+Monologue.meta_keyword         = "rails, programming, monologue, ruby"
+Monologue.twitter_username     = "jipiboily"
+Monologue.twitter_locale       = "en" # "fr"
+Monologue.facebook_like_locale = "en_US" # "fr_CA"
+Monologue.google_plusone_locale = "en"
+Monologue.admin_force_ssl       = false
+Monologue.posts_per_page      = 10
+# Monologue.layout               = "layouts/application"
+# Monologue.google_analytics_id = "YOUR GA CODE"
+
+Monologue.sidebar = ["latests_posts", "latests_tweets"]
